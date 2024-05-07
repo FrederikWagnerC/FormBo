960f616e615750126594e801f191a3ff0817142f
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-05-07 10:35:04 +0200

    Update js
    Update to JS for validating the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const formName = document.getElementById('formName')
 const formLastname = document.getElementById('formLastname')
 const formMail = document.getElementById('formMail')
 const formAge = document.getElementById('formAge')
+const formSelect = document.getElementById('formSelect')
 
 let formNameBoo = false
 let formLastnameBoo = false
@@ -10,10 +11,11 @@ let formMailBoo = false
 let formAgeBoo = false
 
 
-formName.addEventListener('keydown', (event) => {
+formName.addEventListener('keyup', (event) => {
     
-    if (event.key === 'Backspace') {
-        if (formName.value.length -1 >= 2) {
+    const userNameRegex = /^[a-zæøå]+$/gi;
+
+        if (userNameRegex.test(formName.value) && formName.value.length >= 2) {
             formName.classList.add('formValid')
             formName.classList.remove('formInvalid')
             formNameBoo = true
@@ -22,46 +24,25 @@ formName.addEventListener('keydown', (event) => {
             formName.classList.remove('formValid')
             formNameBoo = false
         }
-    } else {
-        if (formName.value.length +1 >= 2) {
-            formName.classList.add('formValid')
-            formName.classList.remove('formInvalid')
-            formNameBoo = true
+    
+})
+
+formLastname.addEventListener('keyup', (event) => {
+    const userNameRegex = /^[a-zæøå]+$/gi;
+
+        if (userNameRegex.test(formLastname.value) && formLastname.value.length >= 2) {
+            formLastname.classList.add('formValid')
+            formLastname.classList.remove('formInvalid')
+            formLastnameBoo = true
         } else {
-            formName.classList.add('formInvalid')
-            formName.classList.remove('formValid')
+            formLastname.classList.add('formInvalid')
+            formLastname.classList.remove('formValid')
             formNameBoo = false
         }
-    }
 
 })
 
-formLastname.addEventListener('keydown', (event) => {
-    if (event.key === 'Backspace') {
-        if (formLastname.value.length-1 >= 2) {
-            formLastname.classList.add('formValid')
-            formLastname.classList.remove('formInvalid')
-            formLastnameBoo = true
-        } else {
-            formLastname.classList.add('formInvalid')
-            formLastname.classList.remove('formValid')
-            formLastnameBoo = false
-        }
-    } else {
-        if (formLastname.value.length +1 >= 2) {
-            formLastname.classList.add('formValid')
-            formLastname.classList.remove('formInvalid')
-            formLastnameBoo = true
-        } else {
-            formLastname.classList.add('formInvalid')
-            formLastname.classList.remove('formValid')
-            formLastnameBoo = false
-        }
-    }
-
-})
-
-formMail.addEventListener('keydown', (event) => {
+formMail.addEventListener('keyup', (event) => {
     let formMailValue = formMail.value.toLowerCase()+event.key;
     const mailRegex = 
         /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
@@ -76,12 +57,10 @@ formMail.addEventListener('keydown', (event) => {
     }
 })
 
-formAge.addEventListener('keydown', (event) => {
-    let formAgeCurrent = parseInt(formAge.value + event.key)
-    if(event.key === 'Backspace') {
-        formAgeCurrent = Math.floor(formAgeCurrent / 10)
-    }
-    if(formAgeCurrent < 0 || formAgeCurrent > 99) {
+formAge.addEventListener('keyup', (event) => {
+    
+    if(formAge.value) {
+    if(formAge.value < 0 || formAge.value > 99) {
         formAge.classList.add('formInvalid')
         formAge.classList.remove('formValid')
         formAgeBoo = false
@@ -89,12 +68,16 @@ formAge.addEventListener('keydown', (event) => {
         formAge.classList.add('formValid')
         formAge.classList.remove('formInvalid')
         formAgeBoo = true
+    }} else {
+        formAge.classList.add('formInvalid')
+        formAge.classList.remove('formValid')
+        formAgeBoo = false
     }
 })
 
 
 formSubmitButton.addEventListener('click', (event) => {
-    event.preventDefault
+    event.preventDefault()
     const userName = document.getElementById('formName').value
     const userLastname = document.getElementById('formLastname').value
     const userMail = document.getElementById('formMail').value
@@ -110,7 +93,8 @@ formSubmitButton.addEventListener('click', (event) => {
         interest: userInterest
     };
 
-    if (formNameBoo === true && formLastname === true && formMail === true && formAgeBoo === true) {
+
+    if (formNameBoo === true && formLastnameBoo === true && formMailBoo === true && formAgeBoo === true && user.interest) {
         console.log('sjå')
     } else {
         console.log('asd')
